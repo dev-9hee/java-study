c8b846638cd0dc044064ad9bd281abc006f09783
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2025-01-13 01:26:11 +0900

    20250113 변수

---
+++ java-start/src/variable/Var4.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c,d; // 한 번에 여러 변수 선언
+    }
+}
---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a); // 주석을 풀면 컴파일 에러 발생
+        // java: variable a might not have been initialized (변수 초기화 오류)
+        // 자바는 변수를 초기화 하도록 강제
+    }
+}
