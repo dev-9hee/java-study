9e046f7b8ac3756a81f030a9beec5b8cfd3f5106
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2025-01-13 01:25:05 +0900

    20250113 알고리즘

---
+++ algorithm-study/src/basic/Solution1000.java
@@ -0,0 +1,12 @@
+package basic;
+import java.util.*;
+
+// 백준 : A + B
+public class Solution1000 {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt(); // A
+    int b = sc.nextInt(); // B
+
+
+}
