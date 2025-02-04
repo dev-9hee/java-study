feedc0a3821358d87f50ffd9004e813a2d1de005
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2025-02-04 23:50:11 +0900

    250204 멤버변수 자동 초기화

---
+++ java-basic/src/ref/InitData.java
@@ -0,0 +1,9 @@
+package ref;
+
+// 인스턴스의 멤버 변수는 인스턴스를 생성할 때 자동으로 초기화
+// 숫자(int) = 0 , boolean = false , 참조형 = null
+// 초기값 직접 지정 가능
+public class InitData {
+    int value1; // 초기화 하지 않음
+    int value2 = 10; // 10으로 초기화
+}
