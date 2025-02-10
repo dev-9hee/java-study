daf3ddc261f5e34fcdbc1e4fb05517a9926c28d6
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2025-02-11 01:34:43 +0900

    250211 백준 | 2577 숫자의 개수

---
+++ algorithm-study/src/basic/Solution2577.java
@@ -0,0 +1,28 @@
+package basic;
+
+import java.util.*;
+
+// 백준 : 숫자의 개수
+public class Solution2577 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[] count = new int[10]; // 0~9까지 쓰인 횟수를 넣을 배열
+        String s = Integer.toString(a * b * c); // A x B x C -> 문자열
+
+        // 각 숫자 문자 갯수 세기
+        for (int i = 0; i < s.length(); i++) {
+            count[Character.getNumericValue(s.charAt(i))]++;
+        }
+
+        // 결과 출력
+        for (int i = 0; i < 10; i++) {
+            System.out.println(count[i]);
+        }
+
+        sc.close();
+    }
+}
