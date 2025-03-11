50ca7cdacc920bf98dcf7c82128e524d9483b7b6
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2025-03-11 22:03:18 +0900

    250311 주석 처리

--- java-basic/src/access/SpeakerMain.java
+++ java-basic/src/access/SpeakerMain.java
@@ -12,8 +12,8 @@ public class SpeakerMain {
         speaker.showVolume();
 
         // 필드에 직접 접근
-        System.out.println("volume 필드 직접 접근 수정");
-        speaker.volume = 200;
-        speaker.showVolume();
+//        System.out.println("volume 필드 직접 접근 수정");
+//        speaker.volume = 200;
+//        speaker.showVolume();
     }
 }
